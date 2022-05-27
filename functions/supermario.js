add1155231f10f190ef9c94c96862d85a9c2c299
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-05-27 11:38:26 -0700

    bug fix

--- functions/supermario.js
+++ functions/supermario.js
@@ -1,7 +1,7 @@
 exports.handler = async () => {
   console.log('function ran')
 
-  const data = { name: 'mario', age 35, job: 'plumber'}
+  const data = { name: 'mario', age: 35, job: 'plumber'}
 
   //return response to b(r)owser
   return {
