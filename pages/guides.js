874d507168c2eafdcd1e9a67dcdc2bf5276a6aa3
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-05-31 11:53:08 -0700

    lesson 10 - client side content guard

--- pages/guides.js
+++ pages/guides.js
@@ -1,9 +1,11 @@
-import { createContext, useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import styles from '../styles/Guides.module.css'
 import AuthContext from '../stores/authContext'
 
 export default function Guides() {
   const { user, authReady } = useContext(AuthContext)
+  const [guides, setGuides] = useState(null)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     if (authReady) {
@@ -12,14 +14,38 @@ export default function Guides() {
           Authorization: 'Bearer ' + user.token.access_token
         }
       })
-        .then(res => res.json())
-        .then(data => console.log(data))
+        .then(res => {
+          if (!res.ok) {
+            throw Error('You must be logged in to view this content')
+          }
+          return res.json()
+        })
+        .then(data => {
+          setGuides(data)
+          setError(null)
+        })
+        .catch((err) => {
+          setError(err.message)
+          setGuides(null)
+        })
     }
   }, [user,authReady])
 
   return (
     <div className={styles.guides}>
-      <h2>All Guides</h2>
+      {!authReady && <div>Loading...</div>}
+      {error && (
+        <div className={styles.error}>
+          <p>{ error }</p>
+        </div>
+      )}
+      {guides && guides.map(guide => (
+        <div key={guide.title} className={styles.card}>
+          <h3>{ guide.title }</h3>
+          <h4>Written by { guide.author }</h4>
+          <p>Mentitum aute offendit eiusmod. Id ipsum sint cillum incididunt est est export sunt labore aliquip, quo ipsum labore ad doctrina, appellat ea laboris. In tempor incurreret singulis, hic qui quorum legam quis. Sint senserit ab praesentibus sed proident quorum export ut dolor.Eu aut eram velit tempor, aliquip cillum fugiat ne duis in quid doctrina id duis magna, ex quis ingeniis illustriora, admodum illum appellat nescius ne ubi o anim voluptate, eu e velit dolore culpa, labore cernantur admodum.</p>
+        </div>
+      ))}
     </div>
   )
 }
